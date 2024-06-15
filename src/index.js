b8d1ceb6c5925ea0b18c5f0fbf66793a29fe676b
kash1ma <ayollo.matvey@gmail.com>
2024-06-15 18:21:01 +0300

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
-import makeDiff from "../src/makeDiff.js";
 import fs from "fs";
 import * as path from "path";
+import makeDiff from "./makeDiff.js";
 import getParse from "./parsers.js";
 import formatDiff from "./formatters/index.js";
-import { cwd } from "node:process";
-const absPath = (filepath) => path.resolve(cwd(), filepath);
+
+const absPath = (filepath) => path.resolve(process.cwd(), filepath).trim();
 
 const getExtention = (filepath) =>
   path.extname(filepath).slice(1).toLowerCase();
 
-const getData = (filepath) => fs.readFileSync(filepath, "utf-8");
+const getData = (filepath) => fs.readFileSync(absPath(filepath), "utf-8");
 
 const genDiff = (filepath1, filepath2, format = "stylish") => {
-  const absolutePathFile1 = absPath(filepath1);
-  const absolutePathFile2 = absPath(filepath2);
-  const extfile1 = getExtention(filepath1);
-  const extfile2 = getExtention(filepath2);
-  const file1 = getData(absolutePathFile1);
-  const file2 = getData(absolutePathFile2);
-  const data1 = getParse(file1, extfile1);
-  const data2 = getParse(file2, extfile2);
+  const data1 = getParse(getData(filepath1), getExtention(filepath1));
+  const data2 = getParse(getData(filepath2), getExtention(filepath2));
 
   const diffTree = makeDiff(data1, data2);
   return formatDiff(diffTree, format);
