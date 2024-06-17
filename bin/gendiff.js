978cab209b0117e4ac9c2d28b569a414ae06892e
kash1ma <ayollo.matvey@gmail.com>
2024-06-17 09:09:03 +0300

    da chto ne tak

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { program } from "commander";
 import genDiff from "../src/index.js";
-import process from "process";
+//import process from "process";
 
 program
   .description("Compares two configuration files and shows a difference.")
